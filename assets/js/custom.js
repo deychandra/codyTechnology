25b536d796bde6f71f93eef67ac90d276438c250
chandra sekhar <deychandrasekhar190@gmail.com>
2024-10-11 22:50:47 +0530

    djfjdf

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,44 +1,55 @@
-$(document).ready(function(){
-       var owl = $(".owl-carousel");
-       owl.owlCarousel({
-         loop: true,
-         margin: 10,
-         nav: false, 
-         dots: false,
-         autoplay: true,
-         autoplayTimeout: 3000,
-         autoplayHoverPause: true,
-         items: 3,
-         onTranslate: progressBar
-       });
+$(document).ready(function () {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    items: 2.4,
+    onTranslate: progressBar
+  });
 
-       $(".customNextBtn").click(function() {
-         owl.trigger('next.owl.carousel');
-       });
-     
-       $(".customPrevBtn").click(function() {
-         owl.trigger('prev.owl.carousel');
-       });
-     
-       // Progress Bar
-       var progress = 0;
-       function progressBar(event) {
-         var autoplayTimeout = owl.data('owl.carousel').options.autoplayTimeout;
-         var interval = 10;  // Progress bar will update every 10ms
-         var total = autoplayTimeout / interval;
-     
-         progress = 0;
-         $(".c-progress-bar").css("width", "0%");
-     
-         var progressInterval = setInterval(function() {
-           progress++;
-           var width = (progress / total) * 100;
-           $(".c-progress-bar").css("width", width + "%");
-     
-           if (progress >= total) {
-             clearInterval(progressInterval);
-           }
-         }, interval);
-       }
-     });
-     
+  $(".customNextBtn").click(function () {
+    owl.trigger('next.owl.carousel');
+  });
+
+  $(".customPrevBtn").click(function () {
+    owl.trigger('prev.owl.carousel');
+  });
+
+  // Progress Bar
+  var progress = 0;
+  function progressBar(event) {
+    var autoplayTimeout = owl.data('owl.carousel').options.autoplayTimeout;
+    var interval = 10;  // Progress bar will update every 10ms
+    var total = autoplayTimeout / interval;
+
+    progress = 0;
+    $(".c-progress-bar").css("width", "0%");
+
+    var progressInterval = setInterval(function () {
+      progress++;
+      var width = (progress / total) * 100;
+      $(".c-progress-bar").css("width", width + "%");
+
+      if (progress >= total) {
+        clearInterval(progressInterval);
+      }
+    }, interval);
+  }
+});
+
+
+
+// Get the elements
+const hamburger = document.querySelector('.toggle-hamburger');
+const header = document.querySelector('header');
+
+// Add event listener for click
+hamburger.addEventListener('click', function () {
+  // Toggle the 'active' class on the <header> element
+  header.classList.toggle('open_nav');
+});
