0f3fb01d97272f56166523caf0d3563c3c3e2f1c
chandra sekhar <deychandrasekhar190@gmail.com>
2024-10-11 20:20:12 +0530

    this is new changes you can open link and view the website

---
+++ assets/js/custom.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+       var owl = $(".owl-carousel");
+       owl.owlCarousel({
+         loop: true,
+         margin: 10,
+         nav: false, 
+         dots: false,
+         autoplay: true,
+         autoplayTimeout: 3000,
+         autoplayHoverPause: true,
+         items: 3,
+         onTranslate: progressBar
+       });
+
+       $(".customNextBtn").click(function() {
+         owl.trigger('next.owl.carousel');
+       });
+     
+       $(".customPrevBtn").click(function() {
+         owl.trigger('prev.owl.carousel');
+       });
+     
+       // Progress Bar
+       var progress = 0;
+       function progressBar(event) {
+         var autoplayTimeout = owl.data('owl.carousel').options.autoplayTimeout;
+         var interval = 10;  // Progress bar will update every 10ms
+         var total = autoplayTimeout / interval;
+     
+         progress = 0;
+         $(".c-progress-bar").css("width", "0%");
+     
+         var progressInterval = setInterval(function() {
+           progress++;
+           var width = (progress / total) * 100;
+           $(".c-progress-bar").css("width", width + "%");
+     
+           if (progress >= total) {
+             clearInterval(progressInterval);
+           }
+         }, interval);
+       }
+     });
+     
